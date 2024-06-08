62f2c485d6bfcf914ab4738b51b376bda3e1b109
Stanislav Levin <stanislav2708@outlook.de>
2024-06-08 08:51:00 +0200

    onclick function for Gamelogic

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const cross = getAnimatedCross(); // Zuweisung für Spieler mit Kreuz Symbol
 const circle = getAnimatedCircle(); // Zuweisung für Spieler mit Kreis Symbol
+let currentPlayer = 'cross';
 
 let fields = [  // Array mit Feldzuständen
-    cross,
-    cross,
-    cross,
     null,
-    circle,
     null,
-    circle,
     null,
-    circle
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
 ];
 
 function render() { // Sobald <body> geladen ist wird die function ausgeführt
@@ -21,7 +22,7 @@ function render() { // Sobald <body> geladen ist wird die function ausgeführt
         for (let j = 0; j < 3; j++) { // Eine zweite for Schleife wird erstellt wo für jedes einzelne i steht
             let index = i * 3 + j; // Eine Rechnung wird erstellt bei der der index für jedes einzelne feld berechnet wird  
             let field = fields[index]; // Ein Feld ist dann ein Element in dem Array fields[Zahl 0-8]
-            html += `<td>${field ? field : ''}</td>`; //Prüft ob ein field den zustanD von Null hat, wenn nein dann kommt ein symbol rein und wenn doch dann ein leeres Feld
+            html += `<td onclick="handleClick(${index})">${field ? field : ''}</td>`; //Prüft ob ein field den zustanD von Null hat, wenn nein dann kommt ein symbol rein und wenn doch dann ein leeres Feld
         }
         html += '</tr>'; // Spalte in der Tabelle wird geschlossen
     }
@@ -33,17 +34,18 @@ function render() { // Sobald <body> geladen ist wird die function ausgeführt
 
 function getAnimatedCircle() {
     return `
-        <svg viewBox="0 0 100 100">
-            <circle cx="50" cy="50" r="40" fill="transparent">
-                <animate 
-                    attributeName="fill" 
-                    from="transparent" 
-                    to="blue" 
-                    dur="2s" 
-                    fill="freeze"
-                    begin="0s" />
-            </circle>
-        </svg>`;
+                <svg viewBox="0 0 100 100">
+                    <circle cx="50" cy="50" r="40" fill="transparent" stroke="blue" stroke-width="5"
+                        stroke-dasharray="251.2" stroke-dashoffset="251.2">
+                        <animate 
+                            attributeName="stroke-dashoffset" 
+                            from="251.2" 
+                            to="0" 
+                            dur="500ms" 
+                            fill="freeze"
+                            begin="0s" />
+                    </circle>
+                </svg>`;
 }
 
 
@@ -55,14 +57,14 @@ function getAnimatedCross() {
                     attributeName="x2" 
                     from="20" 
                     to="80" 
-                    dur="1s" 
+                    dur="500ms" 
                     fill="freeze"
                     begin="0s" />
                 <animate 
                     attributeName="y2" 
                     from="20" 
                     to="80" 
-                    dur="1s" 
+                    dur="500ms" 
                     fill="freeze"
                     begin="0s" />
             </line>
@@ -71,16 +73,30 @@ function getAnimatedCross() {
                     attributeName="x2" 
                     from="80" 
                     to="20" 
-                    dur="1s" 
+                    dur="500ms" 
                     fill="freeze"
-                    begin="0.5s" />
+                    begin="0s" />
                 <animate 
                     attributeName="y2" 
                     from="20" 
                     to="80" 
-                    dur="1s" 
+                    dur="500ms" 
                     fill="freeze"
-                    begin="0.5s" />
+                    begin="0s" />
             </line>
         </svg>`;
+}
+
+
+function handleClick(index) {
+    if (!fields[index]) {
+        if (currentPlayer === 'cross') {
+            fields[index] = cross;
+            currentPlayer = 'circle';
+        } else {
+            fields[index] = circle;
+            currentPlayer = 'cross';
+        }
+        render();
+    }
 }
